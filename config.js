2535991ca6193c0e2b42499b93727939848ed989
bethular <verabethulars@gmail.com>
2026-07-13 14:48:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBDatOJokTydq5P_FLL0adqDG5FimzyHHQ",
+// -----------------------------------------------------------------
+// CONFIGURACIÓN — configuración de Firebase para Punto Electro
+// -----------------------------------------------------------------
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCdDPAMjFDaQgXkWP_ge3iiSXFWygdvl-4",
   authDomain: "punto-electro-f3b73.firebaseapp.com",
   projectId: "punto-electro-f3b73",
   storageBucket: "punto-electro-f3b73.firebasestorage.app",
   messagingSenderId: "435564136124",
   appId: "1:435564136124:web:d961f471696291014672b9"
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
